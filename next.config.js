cb956f5a4fd563cffca05761682cb38a09961874
alkeshghorpade <alkesh26@gmail.com>
2021-02-12 23:30:20 +0530

    added sitemap

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   env: {
     BASE_PATH: process.env.BASE_PATH,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generateSiteMap')
+    }
+    return config
+  }
 };
---
+++ utils/generateSiteMap.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const globby = require('globby')
+
+async function generateSiteMap() {
+  const pages = await globby([
+    'pages/index.js',
+    'content/posts/*.md'
+  ])
+
+  const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages
+            .map(page => {
+              const path = page
+                .replace('pages', '')
+                .replace('.js', '')
+                .replace('content', '')
+                .replace('posts', 'post')
+                .replace('.md', '')
+              const route = path === '/index' ? '' : path
+              return `
+                      <url>
+                          <loc>${`${process.env.BASE_PATH}${route}`}</loc>
+                      </url>
+                  `
+            })
+            .join('')}
+      </urlset>
+  `
+
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+}
+
+generateSiteMap()

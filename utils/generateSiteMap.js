e12b5930c13f95c6bdec68de5e5db1750574af0f
alkeshghorpade <alkesh26@gmail.com>
2022-11-05 10:17:56 +0530

    added sitemap.xml

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -7,7 +7,7 @@ async function generateSiteMap() {
     'content/posts/*.md'
   ])
 
-  const sitemap = `<xml version="1.0" encoding="UTF-8">
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           ${pages
             .map(page => {
@@ -25,8 +25,7 @@ async function generateSiteMap() {
                   `
             })
             .join('')}
-        </urlset>
-      </xml>`
+        </urlset>`
 
   fs.writeFileSync('public/sitemap.xml', sitemap)
 }

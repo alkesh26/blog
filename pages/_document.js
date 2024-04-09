7688f7bfd669b07b111008576378855298700f68
Alkesh Ghorpade <alkesh26@gmail.com>
2024-04-09 21:40:14 +0530

    Added GTM

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,17 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-59XCBD27');
+            `
+            }}
+          />
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
@@ -27,6 +38,18 @@ export default class MyDocument extends Document {
           <TypographyStyle typography={typography} />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-59XCBD27"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden">
+              </iframe>
+              `
+            }}
+          />
           <Main />
           <NextScript />
         </body>
